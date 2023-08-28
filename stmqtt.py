4363126ff915d5b9196245db202b2632886b69c7
PawanKuma <pwnkmr.dst@gmail.com>
2023-08-28 11:30:05 +0530

    Update stmqtt.py

--- stmqtt.py
+++ stmqtt.py
@@ -28,10 +28,17 @@ for message in st.session_state.messages:
 
 counter = 1
 while True:
+    # print(list(names.keys()))
     msg = subscribe.simple(list(names.keys()), qos=0, msg_count=1, retained=True, hostname="20.235.93.174",port=1883, client_id="paho-sub", keepalive=1, will=None, auth={'username':"raiak", 'password':"RAIsw@2023"}, tls=None)
-    if msg!=None and names[msg.topic]:
+    topic = msg.topic
+    if msg!=None and names[topic]:
         message = st.chat_message("assistant")
-        message.write("Topic Name -->  "+ msg.topic + "  msg Counter --> "+ str(counter))
-        message.write("Scheme Name -->  " + schemeids[msg.topic])
+        if topic == "vinworthjasaupur" or topic== "vinworthtest":
+                schemename = schemeids[topic]
+        elif topic== "upapltest":
+#                 # print((json.loads(msg.payload))['trig'])
+                schemename = schemeids[str((json.loads(msg.payload))['trig'])]
+        message.write("Topic Name -->  "+ msg.topic + " || msg Counter --> "+ str(counter))
+        message.write("Scheme Name -->  " + schemename)
         message.write(json.loads(msg.payload))
         counter+=1
